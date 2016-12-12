f435fe7031f9a0d2503ad5c557657eea85d76449
BalandinaElizaveta <balandina_liza@mail.ru>
2016-12-12 14:22:05 +0300

    тесты по постфиксу

--- Баландина Елизавета/base_test/test_tpostfix.cpp
+++ Баландина Елизавета/base_test/test_tpostfix.cpp
@@ -3,6 +3,35 @@
 
 TEST(TPostfix, can_create_postfix)
 {
-  ASSERT_NO_THROW(TPostfix p);
+	string ex = "a+b";
+  ASSERT_NO_THROW(TPostfix p(ex));
 }
 
+TEST(TPostfix, number_one_test)
+{
+	string ex = "a+b";
+	TPostfix p(ex);
+	string res = "ab+";
+	EXPECT_EQ(res,p.ToPostfix());
+}
+TEST(TPostfix, number_two_test)
+{
+	string ex = "a+b/d-c";
+	TPostfix p(ex);
+	string res = "abd/+c-";
+	EXPECT_EQ(res, p.ToPostfix());
+}
+TEST(TPostfix, number_three_test)
+{
+	string ex = "(a+b)/k-c";
+	TPostfix p(ex);
+	string res = "ab+k/c-";
+	EXPECT_EQ(res, p.ToPostfix());
+}
+TEST(TPostfix, number_four_test)
+{
+	string ex = "(a+b)/(d*c)";
+	TPostfix p(ex);
+	string res = "ab+dc*/";
+	EXPECT_EQ(res, p.ToPostfix());
+}
