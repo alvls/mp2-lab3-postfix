8b966fb14162cf5cf98233d2d4d86d7bbb21a262
Anilmal <baranovart231999@gmail.com>
2018-11-01 19:15:44 +0300

    Еще работаем

--- Сысоев/base/postfix.h
+++ Сысоев/base/postfix.h
@@ -12,27 +12,19 @@ class TPostfix
   string infix;
   string postfix;
 public:
+  TPostfix()
+  {
+   infix = "a + b";
+  }
   TPostfix(string _infix)
   {
-   // infix = "a + b";
 	  infix = _infix;
   }
   string GetInfix() { return infix; }
   string GetPostfix() { return postfix; }
-  string ToPostfix()
-  {
-	  TStack<char> operations(infix.size);
-	  TStack<char> symbols(infix.size);
-	  for (int i = 0; i < infix.size; i++)
-	  {
-		  if (!isalpha(infix[i]))
-			  operations.Put(infix[i]);
-		  else
-			  symbols.Put(infix[i]);
-		//  if( infix[i]=='(' || infix[i]==')')
-	  }
-  }
-  double Calculate(); // Ввод переменных, вычисление по постфиксной форме
+  int Priority(char sym);
+  string ToPostfix();
+  double Calculate(double *values);
 };
 
 #endif
