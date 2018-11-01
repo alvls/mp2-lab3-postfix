6f02561878ec0380981a073dcd208ea13a92b986
Anilmal <baranovart231999@gmail.com>
2018-11-01 21:57:07 +0300

    gtest.lib все еще проблема

---
+++ Баранов/base/postfix.cpp
@@ -0,0 +1,64 @@
+﻿#include "postfix.h"
+#include "stack.h"
+
+string TPostfix::ToPostfix()
+{
+	TStack<char> operations(infix.size());
+	TStack<char> symbols(infix.size());
+	TStack<char> operations1(infix.size());
+	TStack<char> symbols1(infix.size());
+	for (int i = 0; i < infix.size(); i++)
+	{
+		if (!isalpha(infix[i]))
+			operations.Put(infix[i]);
+		if (infix[i] == '(')
+		{
+			i++;
+			while (infix[i] != ')')
+			{
+				if (!isalpha(infix[i]))
+					operations1.Put(infix[i]);
+				else
+					symbols1.Put(infix[i]);
+				i++;
+			}
+		}
+
+		else
+			symbols.Put(infix[i]);
+	}
+	while (!symbols.IsEmpty())
+		postfix += symbols1.Get();
+	while (!operations.IsEmpty())
+		postfix += operations1.Get();
+	while (!symbols.IsEmpty())
+		postfix += symbols.Get();
+	while (!operations.IsEmpty())
+		postfix += operations.Get();
+}
+int TPostfix::Priority(char sym)
+{
+	if (sym == '*' || sym == '/')
+		return 2;
+	if (sym == '+' || sym == '-')
+		return 1;
+}
+double TPostfix:: Calculate(double *values)
+{
+	TStack<double>arg(sizeof(values) / sizeof(*values));
+	for (size_t i = 0; i < sizeof(values) / sizeof(*values); i++)
+	{
+		arg.Put(values[i]);
+	}
+	double res;
+	while (!arg.IsEmpty())
+	{
+		for (int i = 0; i < postfix.size(); i++)
+		{
+			if (isalpha(postfix[i]))
+				postfix[i] = arg.Get();
+		}
+	}
+	res = atof(postfix.c_str());
+	return res; // Ввод переменных, вычисление по постфиксной 
+}
---
+++ Баранов/base/stack.h
@@ -0,0 +1,64 @@
+﻿#ifndef __STACK_H__
+#define __STACK_H__
+
+const int MaxStackSize = 100;
+
+template <class T>
+class TStack
+{
+  T *pMem;
+  int size;
+  int top;
+public:
+  TStack(int _size)
+  {
+    size = _size;
+    top = -1;
+    if ((size < 1) || (size > MaxStackSize))
+      throw size;
+    pMem = new T[size];
+  }
+  ~TStack()
+  {
+    delete [] pMem;
+  }
+  void Put(const T &elem)
+  {
+	  if (top < size)
+		  pMem[top++] = elem;
+	  else
+		  throw size;
+  }
+  T Get()
+  {
+	  if (top < 0)
+		  throw top;
+	  else
+	  {
+		  T elem;
+		  elem = pMem[top];
+		  pMem[--top];
+		  return elem;
+	  }
+  }
+  bool IsEmpty()
+  {
+	  for (int i = 0; i < size; i++)
+	  {
+		  if (pMem[i] != 0)
+			  return false;
+	  }
+	  return true;
+  }
+  bool IsFull()
+  {
+	  for (int i = 0; i < size; i++)
+	  {
+		  if (pMem[i] == 0)
+			  return false;
+	  }
+	  return true;
+  }
+};
+
+#endif
