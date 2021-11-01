01854008824803e2cc430738bc10af1cddf0cfcb
Sergey Zagrebin <sergey.127.0.0.1@ya.ru>
2021-11-01 11:55:26 +0300

    Tests Needed v.11

--- Загребин/base_test/test_tpostfix.cpp
+++ Загребин/base_test/test_tpostfix.cpp
@@ -9,8 +9,8 @@ TEST(TPostfix, can_create_postfix)
 TEST(TPostfix, can_turn_to_postfix)
 {
 	TPostfix p;
-	p.SetInfix("2 + x");
-	EXPECT_EQ("2 x + ", p.GetPostfix());
+	p.SetInfix("2 / ( x + 3 ) ");
+	EXPECT_EQ("2 x 3 + / ", p.GetPostfix());
 }
 
 TEST(TPostfix, can_calculate_plus_postfix)
@@ -56,31 +56,31 @@ TEST(TPostfix, can_calculate_power_postfix)
 TEST(TPostfix, can_calculate_exp_postfix)
 {
 	TPostfix p;
-	p.SetInfix("exp 0");
+	p.SetInfix("exp 1");
 	p.GetPostfix();
-	EXPECT_EQ(1, p.Calculate());
+	EXPECT_EQ(exp(1), p.Calculate());
 }
 
 TEST(TPostfix, can_calculate_natural_log_postfix)
 {
 	TPostfix p;
-	p.SetInfix("ln 1");
+	p.SetInfix("ln 2");
 	p.GetPostfix();
-	EXPECT_EQ(0, p.Calculate());
+	EXPECT_EQ(log(2), p.Calculate());
 }
 
 TEST(TPostfix, can_calculate_sqrt_postfix)
 {
 	TPostfix p;
-	p.SetInfix("sqrt 4");
+	p.SetInfix("sqrt 2");
 	p.GetPostfix();
-	EXPECT_EQ(2, p.Calculate());
+	EXPECT_EQ(sqrt(2), p.Calculate());
 }
 
 TEST(TPostfix, can_calculate_sin_postfix)
 {
 	TPostfix p;
-	p.SetInfix("sin 0");
+	p.SetInfix("sin 1");
 	p.GetPostfix();
-	EXPECT_EQ(0, p.Calculate());
+	EXPECT_EQ(sin(1), p.Calculate());
 }
